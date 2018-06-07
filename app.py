2c124a59bfe42164bf11dfe9cc29b3d4c0955b35
tmichalowski <tomasz.michalowski@atos.net>
2018-06-07 09:16:36 +0200

    small update

--- app.py
+++ app.py
@@ -17,15 +17,15 @@ def execute_command(command):
 
 
 def do_screen_capturing(url, screen_path, width, height):
-   print "Capturing screen.."
-   print screen_path
+   print ("Capturing screen..")
    driver = webdriver.PhantomJS()
    #driver = webdriver.Firefox()
    # it save service log file in same directory
    # if you want to have log file stored else where
    # initialize the webdriver.PhantomJS() as
    # driver = webdriver.PhantomJS(service_log_path='/var/log/phantomjs/ghostdriver.log')
-   driver.set_script_timeout(30)
+  # driver.set_script_timeout(30)
+
    if width and height:
       driver.set_window_size(width, height)
    timeout = 10
@@ -34,16 +34,14 @@ def do_screen_capturing(url, screen_path, width, height):
       element_present = EC.presence_of_element_located((By.ID, 'element_id'))
       WebDriverWait(driver, timeout).until(element_present)
       element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.ID, 'element_id')))
-
    except TimeoutException:
-      print "Timed out waiting for page to load"
-     # pass
+      print ("Timed out waiting for page to load")
 
    driver.save_screenshot(screen_path)
-
+#   driver.close()
 
 def do_crop(params):
-   print "Croping captured image.."
+   print ("Croping captured image..")
    command = [
       'convert',
       params['screen_path'],
@@ -54,7 +52,7 @@ def do_crop(params):
 
 
 def do_thumbnail(params):
-   print "Generating thumbnail from croped captured image.."
+   print ("Generating thumbnail from croped captured image..")
    command = [
       'convert',
       params['crop_path'],
@@ -69,7 +67,7 @@ def get_screen_shot(**kwargs):
    url = kwargs['url']
    width = int(kwargs.get('width', 1024)) # screen width to capture
    height = int(kwargs.get('height', 768)) # screen height to capture
-   filename = kwargs.get('filename', 'screen.png') # file name e.g. screen.png
+   filename = kwargs.get('filename', 'screen') # file name e.g. screen.png
    path = kwargs.get('path', ROOT) # directory path to store screen
    file_date = kwargs.get('date', str(today))
    crop = kwargs.get('crop', False) # crop the captured screen
@@ -81,14 +79,14 @@ def get_screen_shot(**kwargs):
    thumbnail_width = int(kwargs.get('thumbnail_width', width)) # the width of thumbnail
    thumbnail_height = int(kwargs.get('thumbnail_height', height)) # the height of thumbnail
    thumbnail_replace = kwargs.get('thumbnail_replace', False) # does thumbnail image replace crop image?
-   filename2="_".join([str(file_date),filename])
-   print "kwargs",kwargs
-   screen_path = abspath(path, filename2)
-   print screen_path
+   filename="_".join([str(file_date),filename])
+   filename="".join([filename,".png"])
+   screen_path = abspath(path, filename)
    crop_path = thumbnail_path = screen_path
 
-   if thumbnail and not crop:
-      raise Exception, 'Thumnail generation requires crop image, set crop=True'
+#   if thumbnail and not crop:
+#      raise Exception,
+#         'Thumnail generation requires crop image, set crop=True'
 
    do_screen_capturing(url, screen_path, width, height)
 
--- web.py
+++ web.py
@@ -16,7 +16,7 @@ def snapshooter():
 
    width = request.args.get('width', 1024) # screen width to capture
    height = request.args.get('height', 768) # screen height to capture
-   filename = request.args.get('filename', 'screen.png') # file name e.g. screen.png
+   filename = request.args.get('filename', 'screen') # file name e.g. screen.png
    path = request.args.get('path', ROOT ) # directory path to store screen
    crop = request.args.get('crop', False) # crop the captured screen
    crop_width = request.args.get('crop_width',width) # the width of crop screen
@@ -31,9 +31,19 @@ def snapshooter():
    screen_path = get_screen_shot(url=url, crop=crop, thumbnail=crop, filename=filename,width=width, height=height, file_date=file_date, path=path)
    return '''
 <h1>Screenshoting URL: {}</h1>
-
+<p><strong>Possible parameters:</strong></p>
+<p><strong>Required:</strong></p>
+<p>url = URL of page to save</p>
+<p><strong>Optional:</strong></p>
+<p><br /> width = default (1024)&nbsp;# screen width to capture<br /> height = default (768)&nbsp; # screen height to capture<br /> filename = default ('screen.png') # file name e.g. screen.png<br /> path = default (ROOT) # directory path to store screen</p>
+<p><br /> crop = default (False) # crop the captured screen<br /> crop_width =(width)# the width of crop screen<br /> crop_height = (height) # the height of crop screen<br /> crop_replace = (False) # does crop image replace original screen capture?</p>
+<p>thumbnail = default (False) # generate thumbnail from screen, requires crop=True<br /> thumbnail_width = (width) # the width of thumbnail<br /> thumbnail_height = (height) # the height of thumbnail<br /> thumbnail_replace = (False) # does thumbnail image replace crop image?</p>
+<p>Created by</p>
+<p>Tomasz Michalowski</p>
+<p>&nbsp;</p>
 '''.format(url)
 
 
 if __name__ == '__main__':
    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+
