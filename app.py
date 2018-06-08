bd72a12e8a91412326e9c4bdfcce0050d8c846dd
tmichalowski <tomasz.michalowski@atos.net>
2018-06-08 10:01:34 +0200

    Change of engine
    Change to google engine.
    Don't use PhantoJS.
    
    Additionaly move some variables to configuration file.

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import datetime
+import json
+with open('config.json') as json_data_file:
+   config = json.load(json_data_file)
+
 abspath = lambda *p: os.path.abspath(os.path.join(*p))
-ROOT = abspath(os.path.dirname(__file__))
+#ROOT = abspath(os.path.dirname(__file__))
+ROOT = "/root/flask_nodejs/img"
 today=datetime.date.today()
 
 def execute_command(command):
@@ -18,16 +23,23 @@ def execute_command(command):
 
 def do_screen_capturing(url, screen_path, width, height):
    print ("Capturing screen..")
-   driver = webdriver.PhantomJS()
-   #driver = webdriver.Firefox()
-   # it save service log file in same directory
-   # if you want to have log file stored else where
-   # initialize the webdriver.PhantomJS() as
-   # driver = webdriver.PhantomJS(service_log_path='/var/log/phantomjs/ghostdriver.log')
-  # driver.set_script_timeout(30)
+   print ("url=",url)
+   print ("screen_path=",screen_path)
+   print ("width",width)
+   print ("height",height)
+   options = webdriver.ChromeOptions()
+   options.binary_location = ("/usr/bin/google-chrome-stable")
 
-   if width and height:
-      driver.set_window_size(width, height)
+   for i in config["app"]["chrome_options"]:
+      options.add_argument(i)
+   #options.add_argument("disable-gpu")# disabling infobars
+   #options.add_argument("disable-infobars")# disabling infobars
+   #options.add_argument("--no-sandbox")
+   #options.add_argument("--disable-extensions")
+   #options.add_argument("--headless")
+   driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=options) #executable_path="/usr/local/bin/geckodriver")
+   #if width and heigh:
+   driver.set_window_size(width, height)
    timeout = 10
    driver.get(url)
    try:
@@ -38,7 +50,8 @@ def do_screen_capturing(url, screen_path, width, height):
       print ("Timed out waiting for page to load")
 
    driver.save_screenshot(screen_path)
-#   driver.close()
+  # display.stop()
+   driver.close()
 
 def do_crop(params):
    print ("Croping captured image..")
@@ -65,8 +78,8 @@ def do_thumbnail(params):
 
 def get_screen_shot(**kwargs):
    url = kwargs['url']
-   width = int(kwargs.get('width', 1024)) # screen width to capture
-   height = int(kwargs.get('height', 768)) # screen height to capture
+   width = int(kwargs.get('width', 1920)) # screen width to capture
+   height = int(kwargs.get('height', 1080)) # screen height to capture
    filename = kwargs.get('filename', 'screen') # file name e.g. screen.png
    path = kwargs.get('path', ROOT) # directory path to store screen
    file_date = kwargs.get('date', str(today))
@@ -84,9 +97,8 @@ def get_screen_shot(**kwargs):
    screen_path = abspath(path, filename)
    crop_path = thumbnail_path = screen_path
 
-#   if thumbnail and not crop:
-#      raise Exception,
-#         'Thumnail generation requires crop image, set crop=True'
+   if thumbnail and not crop:
+      raise Exception('Thumnail generation requires crop image, set crop=True')
 
    do_screen_capturing(url, screen_path, width, height)
 
@@ -111,14 +123,12 @@ def get_screen_shot(**kwargs):
 if __name__ == '__main__':
    '''
        Requirements:
-       Install NodeJS
-       Using Node's package manager install phantomjs: npm -g install phantomjs
        install selenium (in your virtualenv, if you are using that)
-       install imageMagick
-       add phantomjs to system path (on windows)
+       install chrome and chromedriver
    '''
 
-   url = 'http://play.grafana-zabbix.org/d/000000003/grafana-zabbix-demo?orgId=2'
+   url = "http://play.grafana-zabbix.org/d/000000024/triggers?orgId=2"
+#   url = "http://play.grafana-zabbix.org/d/000000011/grafana-zabbix-org?orgId=2"
    screen_path, crop_path, thumbnail_path = get_screen_shot(
       url=url, filename='sof.png',
       crop=True, crop_replace=False,
--- web.py
+++ web.py
@@ -1,11 +1,14 @@
 from flask import Flask, request #import main Flask class and request object
 import sys
 from selenium import webdriver
-from app import get_screen_shot
+from app3 import get_screen_shot
 import os
 import datetime
-abspath = lambda *p: os.path.abspath(os.path.join(*p))
-ROOT = "/root/flask_nodejs/screenshots"
+import json
+with open('config.json') as json_data_file:
+   config = json.load(json_data_file)
+
+
 today=str(datetime.date.today())
 app = Flask(__name__) #create the Flask app
 
@@ -14,20 +17,19 @@ app = Flask(__name__) #create the Flask app
 def snapshooter():
    url = request.args.get('url') #if key doesn't exist, returns None
 
-   width = request.args.get('width', 1024) # screen width to capture
-   height = request.args.get('height', 768) # screen height to capture
-   filename = request.args.get('filename', 'screen') # file name e.g. screen.png
-   path = request.args.get('path', ROOT ) # directory path to store screen
+   width = request.args.get('width', config['web']['default']['width']) # screen width to capture
+   height = request.args.get('height', config['web']['default']['height']) # screen height to capture
+   filename = request.args.get('filename', config['web']['default']['filename']) # file name e.g. screen.png
+   path = request.args.get('path', config['web']['default']['path'] ) # directory path to store screen
    crop = request.args.get('crop', False) # crop the captured screen
-   crop_width = request.args.get('crop_width',width) # the width of crop screen
-   crop_height = request.args.get('crop_height',height) # the height of crop screen
+   crop_width = request.args.get('crop_width',config['web']['default']['width']) # the width of crop screen
+   crop_height = request.args.get('crop_height',config['web']['default']['height']) # the height of crop screen
    crop_replace = request.args.get('crop_replace',False) # does crop image replace original screen capture?
-   file_date = request.args.get('date',today)
+   file_date = request.args.get('date',str(datetime.date.today()))
    thumbnail = request.args.get('thumbnail', False) # generate thumbnail from screen, requires crop=True
-   thumbnail_width = request.args.get('thumbnail_width',width) # the width of thumbnail
-   thumbnail_height = request.args.get('thumbnail_height',height) # the height of thumbnail
+   thumbnail_width = request.args.get('thumbnail_width',config['web']['default']['width']) # the width of thumbnail
+   thumbnail_height = request.args.get('thumbnail_height',config['web']['default']['height']) # the height of thumbnail
    thumbnail_replace = request.args.get('thumbnail_replace',False) # does thumbnail image replace crop image?
-
    screen_path = get_screen_shot(url=url, crop=crop, thumbnail=crop, filename=filename,width=width, height=height, file_date=file_date, path=path)
    return '''
 <h1>Screenshoting URL: {}</h1>
@@ -46,4 +48,5 @@ def snapshooter():
 
 if __name__ == '__main__':
    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+
 
